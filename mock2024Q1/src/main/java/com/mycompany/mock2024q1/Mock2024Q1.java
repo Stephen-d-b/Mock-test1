e2ba03168640827a6317a791ab74d4203c99a83c
Stephen-d-b <st10472491@vcconnect.edu.za>
2026-09-22 10:37:34 +0200

    This is the 2024 Mock test examples

---
+++ mock2024Q1/src/main/java/com/mycompany/mock2024q1/Mock2024Q1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mock2024q1;
+
+/**
+ *
+ * @author NLS - SMP
+ */
+public class Mock2024Q1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ mock2024Q2/src/main/java/com/mycompany/mock2024q2/Mock2024Q2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mock2024q2;
+
+/**
+ *
+ * @author NLS - SMP
+ */
+public class Mock2024Q2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
