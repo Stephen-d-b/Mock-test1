1277a5a82591b1ffba2a5e605aa1868f048ed2d2
simon-is-a-dev <simonswart91@gmail.com>
2026-09-24 13:12:00 +0200

    SIMON First commit

---
+++ SIMONCODE/2020/mockTest/src/main/java/com/mycompany/mocktest/MockTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mocktest;
+
+/**
+ *
+ * @author simon
+ */
+public class MockTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
